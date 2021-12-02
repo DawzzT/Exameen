83627572f017b7d1413f225d742b88e0e8056ef1
Walterddt1M1 <walddt05@gmail.com>
2021-12-02 09:30:37 -0600

    MOdelsssssssssss

---
+++ INfraestructura/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructura
+{
+    public class Model <T>
+    {
+        List<T> checherereque;
+
+        public void add(T t)
+        {
+            checherereque.Add(t);
+        }
+    }
+}
